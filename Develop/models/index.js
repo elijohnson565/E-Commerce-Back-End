2a5ad1a016aecc1cfc547cee8892f8bf750866e3
Elijah Johnson <elijohnson565@gmail.com>
2022-05-27 18:17:17 -0700

    finished models

---
+++ Develop/models/index.js
@@ -0,0 +1,35 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL'
+});
+
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
+
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
